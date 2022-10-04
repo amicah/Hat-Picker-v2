78fdd7f85c21cbfbe1298a66291f63de2f1ddace
A Micah McClain <a.micah.mcclain@gmail.com>
2022-10-04 16:29:29 -0700

    Add files via upload
    Initial file uploads.

---
+++ Form1.Designer.cs
@@ -0,0 +1,317 @@
+﻿namespace HatPicker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.addButton = new System.Windows.Forms.Button();
+            this.entryBox = new System.Windows.Forms.TextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.deleteButton = new System.Windows.Forms.Button();
+            this.drawButton = new System.Windows.Forms.Button();
+            this.hatBox = new System.Windows.Forms.ListBox();
+            this.drawBox = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.clearToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.copyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 84F));
+            this.tableLayoutPanel1.Controls.Add(this.addButton, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.entryBox, 0, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 27);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 38F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(659, 38);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // addButton
+            // 
+            this.addButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.addButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.addButton.Location = new System.Drawing.Point(578, 3);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(78, 27);
+            this.addButton.TabIndex = 1;
+            this.addButton.Text = "Add";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // entryBox
+            // 
+            this.entryBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.entryBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.entryBox.Location = new System.Drawing.Point(3, 3);
+            this.entryBox.Name = "entryBox";
+            this.entryBox.Size = new System.Drawing.Size(569, 26);
+            this.entryBox.TabIndex = 0;
+            this.entryBox.Text = "Name";
+            this.entryBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.entryBox_KeyPress);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.editToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(683, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.saveToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.S)));
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(177, 6);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel2.ColumnCount = 2;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Controls.Add(this.deleteButton, 0, 2);
+            this.tableLayoutPanel2.Controls.Add(this.drawButton, 0, 2);
+            this.tableLayoutPanel2.Controls.Add(this.hatBox, 0, 1);
+            this.tableLayoutPanel2.Controls.Add(this.drawBox, 1, 1);
+            this.tableLayoutPanel2.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.label2, 1, 0);
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(12, 71);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 3;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 33F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(659, 374);
+            this.tableLayoutPanel2.TabIndex = 2;
+            // 
+            // deleteButton
+            // 
+            this.deleteButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.deleteButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.deleteButton.Location = new System.Drawing.Point(246, 344);
+            this.deleteButton.Name = "deleteButton";
+            this.deleteButton.Size = new System.Drawing.Size(80, 27);
+            this.deleteButton.TabIndex = 3;
+            this.deleteButton.Text = "Delete";
+            this.deleteButton.UseVisualStyleBackColor = true;
+            this.deleteButton.Click += new System.EventHandler(this.deleteButton_Click);
+            // 
+            // drawButton
+            // 
+            this.drawButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.drawButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.drawButton.Location = new System.Drawing.Point(576, 344);
+            this.drawButton.Name = "drawButton";
+            this.drawButton.Size = new System.Drawing.Size(80, 27);
+            this.drawButton.TabIndex = 4;
+            this.drawButton.Text = "Draw";
+            this.drawButton.UseVisualStyleBackColor = true;
+            this.drawButton.Click += new System.EventHandler(this.drawButton_Click);
+            // 
+            // hatBox
+            // 
+            this.hatBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.hatBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.hatBox.FormattingEnabled = true;
+            this.hatBox.ItemHeight = 20;
+            this.hatBox.Location = new System.Drawing.Point(3, 23);
+            this.hatBox.Name = "hatBox";
+            this.hatBox.Size = new System.Drawing.Size(323, 304);
+            this.hatBox.TabIndex = 2;
+            // 
+            // drawBox
+            // 
+            this.drawBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.drawBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.drawBox.FormattingEnabled = true;
+            this.drawBox.ItemHeight = 20;
+            this.drawBox.Location = new System.Drawing.Point(332, 23);
+            this.drawBox.Name = "drawBox";
+            this.drawBox.Size = new System.Drawing.Size(324, 304);
+            this.drawBox.TabIndex = 4;
+            this.drawBox.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Inputs:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(332, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Outputs:";
+            // 
+            // editToolStripMenuItem
+            // 
+            this.editToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.clearToolStripMenuItem,
+            this.copyToolStripMenuItem});
+            this.editToolStripMenuItem.Name = "editToolStripMenuItem";
+            this.editToolStripMenuItem.Size = new System.Drawing.Size(39, 20);
+            this.editToolStripMenuItem.Text = "Edit";
+            // 
+            // clearToolStripMenuItem
+            // 
+            this.clearToolStripMenuItem.Name = "clearToolStripMenuItem";
+            this.clearToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.clearToolStripMenuItem.Text = "Clear";
+            this.clearToolStripMenuItem.Click += new System.EventHandler(this.clearToolStripMenuItem_Click);
+            // 
+            // copyToolStripMenuItem
+            // 
+            this.copyToolStripMenuItem.Name = "copyToolStripMenuItem";
+            this.copyToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.C)));
+            this.copyToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.copyToolStripMenuItem.Text = "Copy";
+            this.copyToolStripMenuItem.Click += new System.EventHandler(this.copyToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(683, 457);
+            this.Controls.Add(this.tableLayoutPanel2);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.MinimumSize = new System.Drawing.Size(500, 250);
+            this.Name = "Form1";
+            this.Text = "HatPicker";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.TextBox entryBox;
+        private System.Windows.Forms.Button deleteButton;
+        private System.Windows.Forms.Button drawButton;
+        private System.Windows.Forms.ListBox hatBox;
+        private System.Windows.Forms.ListBox drawBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem clearToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem copyToolStripMenuItem;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,196 @@
+﻿/*
+    HatPicker v2.0
+    Author: A Micah McClain
+    Email: a.micah.mcclain@gmail.com
+    Date: 10/4/2022
+    Description:
+        An updated version of my old HatPicker program. Enter names into a digital Hat, and draw them! Perfect for
+        Secret Santa-type situations. Implements new features as well as bugfixes.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.IO;
+
+namespace HatPicker
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            saveToolStripMenuItem.Enabled = false;
+            copyToolStripMenuItem.Enabled = false;
+        }
+
+        private Random rand = new Random();
+        private int listSize;
+        private List<string> saveList;
+        private StreamWriter sw;
+
+        // Add item to hatBox and clear the entryBox
+        private void insertItem(string item)
+        {
+            addButton.Enabled = false;
+            if (item != "Name" && item != "")
+            {
+                if (!hatBox.Items.Contains(item))
+                {
+                    hatBox.Items.Add(item);
+                }
+                else
+                {
+                    MessageBox.Show("Duplicate entry.", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+            }
+            entryBox.Clear();
+            addButton.Enabled = true;
+        }
+
+        // Adds item to hatBox by pressing 'Enter'
+        private void entryBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == ((char)Keys.Return))
+            {
+                insertItem(entryBox.Text);
+                e.Handled = true;
+            }
+        }
+
+        // Adds item to hatBox using the addButton
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            insertItem(entryBox.Text);
+        }
+
+        // Removes selected item from hatBox
+        private void deleteButton_Click(object sender, EventArgs e)
+        {
+            deleteButton.Enabled = false;
+            if (hatBox.SelectedIndex != -1) 
+            { 
+                hatBox.Items.RemoveAt(hatBox.SelectedIndex);
+            }
+            deleteButton.Enabled = true;
+        }
+
+        // Shuffles the Hat, draws names, and display it in the drawBox
+        private void drawButton_Click(object sender, EventArgs e)
+        {
+            drawButton.Enabled = false;
+            this.listSize = hatBox.Items.Count;
+            this.saveList = new List<string>(this.listSize);
+            List<string> hatList = new List<string>(this.listSize);
+            List<string> drawList = new List<string>(this.listSize);
+
+            if (this.listSize > 0)
+            {
+                // Clear previous contents of outputBox and get the contents of inputsBox
+                drawBox.Items.Clear();
+                for (int i = 0; i < this.listSize; i++)
+                {
+                    hatList.Add(hatBox.Items[i].ToString());
+                }
+                // Shuffle the contents of inputsBox and display in outputsBox
+                bool reroll = true;
+                int counter = 0;
+                while (reroll)
+                {
+                    int rNum = this.rand.Next(0, this.listSize);
+                    // Last pick
+                    if (counter == (this.listSize - 1))
+                    {
+                        // Start over if the last person picks themself
+                        if (!drawList.Contains(hatList[counter]))
+                        {
+                            counter = 0;
+                            drawList.Clear();
+                        }
+                        // Otherwise add the last pick and end the reroll
+                        else if (!drawList.Contains(hatList[rNum]))
+                        {
+                            drawList.Add(hatList[rNum]);
+                            reroll = false;
+                        }
+                    }
+                    // Otherwise, pick a random name. If it's invalid, try again.
+                    else
+                    {
+                        if (counter != rNum && !drawList.Contains(hatList[rNum]))
+                        {
+                            drawList.Add(hatList[rNum]);
+                            counter++;
+                        }
+                    }
+                }
+                // Display the shuffled items and prepare saveList
+                for (int i = 0; i < this.listSize; i++)
+                {
+                    drawBox.Items.Add(drawList[i]);
+                    this.saveList.Add(hatBox.Items[i].ToString() + " -> " + drawBox.Items[i].ToString());
+                }
+            }
+            saveToolStripMenuItem.Enabled = true;
+            copyToolStripMenuItem.Enabled = true;
+            drawButton.Enabled = true;
+        }
+
+        // Close program via menu
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        // Save file as .txt formatted as 'Name1 -> Name2' list
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFile = new SaveFileDialog();
+            saveFile.Filter = "Text File (*.txt)|*.txt";
+            saveFile.Title = "Save text file";
+            if (saveFile.ShowDialog() == DialogResult.OK)
+            {
+                sw = new StreamWriter(saveFile.FileName);
+                for (int i = 0; i < listSize; i++)
+                {
+                    sw.WriteLine(saveList[i].ToString());
+                }
+                sw.Close();
+            }
+        }
+
+        // Clear the hatBox and the drawBox
+        private void clearToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            hatBox.Items.Clear();
+            drawBox.Items.Clear();
+        }
+
+        // Copy text formatted as 'Name1 -> Name2' list to clipboard
+        private void copyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string copyText = "";
+            for (int i = 0; i < listSize; i++)
+            {
+                copyText += saveList[i].ToString();
+                if (i != listSize - 1)
+                {
+                    copyText += "\n";
+                }
+            }
+            Clipboard.SetText(copyText);
+        }
+
+        // Small info box
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(
+                "Add names to the digital hat,\nthen shuffle and draw names." +
+                "\n\n\nAuthor:\nA Micah McClain\na.micah.mcclain@gmail.com",
+                "HatPicker v2.0",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Information
+            );
+        }
+    }
+}
